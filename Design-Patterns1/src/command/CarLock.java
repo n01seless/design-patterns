1ef3ffcac2832247e5a95eeefbb5c4d719598588
n01seless <63344382+n01seless@users.noreply.github.com>
2020-04-28 18:14:57 +0300

    Add files via upload

---
+++ Design-Patterns1/src/command/CarLock.java
@@ -0,0 +1,15 @@
+package command;
+
+public class CarLock {
+private Boolean lightsOn;
+	
+	public void switchOn() {
+		System.out.println("Lights are on");
+		this.lightsOn = true;
+	}
+	
+	public void switchOff() {
+		System.out.println("Lights are off");
+		this.lightsOn = false;
+	}
+}
